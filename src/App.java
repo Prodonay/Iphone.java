256cb35144ed1908600095b1bc982e76b96ac280
prodonay.online <prodonay@gmail.com>
2026-08-27 00:22:57 +0000

    Atualiza projeto iPhone e configura gitignore

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+
+        iPhone myiPhone = new iPhone();
+
+        myiPhone.displayPage();
+        myiPhone.addPage();
+        myiPhone.updatePage();
     }
 }
